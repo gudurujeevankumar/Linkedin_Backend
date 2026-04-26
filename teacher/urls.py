3ceceb64b180d632db935b355a614dfc58e73a6f
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-04-26 12:34:04 +0530

    Create teacher app templates with correct fields (empid, name, email, subject) and add app_name namespace to urls

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'teacher'
+
 urlpatterns = [
     path('details/', views.teacher_details),
     path('insert/', views.insert, name="insert"),

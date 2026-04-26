2d392955d5817d916af6a1b7b9ceb9de03f8cdbe
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-04-26 10:50:51 +0530

    Model is created fro student table with roll,name,age,email,branch

--- student/models.py
+++ student/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    roll = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    email = models.EmailField()
+    branch = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name 
